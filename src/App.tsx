865dca2d77225aca25845d67001cad0ad8bd4bd6
Andrew Meyer <andrew.meyer@mongodb.com>
2021-06-08 10:50:03 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import type {ReactNode} from 'react';
+ import {
+   SafeAreaView,
+ } from 'react-native'
+
+ const App: () => ReactNode = () => {
+   return (
+     <SafeAreaView>
+     </SafeAreaView>
+   );
+ };
+
+
+ export default App;
